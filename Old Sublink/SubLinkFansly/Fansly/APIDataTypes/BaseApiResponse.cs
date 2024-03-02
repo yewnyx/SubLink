c6396f2b4da89a5b55edfb2e44d53f2541eddb68
Laura Braker <thibmorozier@live.nl>
2024-03-02 23:52:01 +0100

    Cleanup old SubLink kept for reference

--- Old Sublink/SubLinkFansly/Fansly/APIDataTypes/BaseApiResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Text.Json.Serialization;
-
-namespace xyz.yewnyx.SubLink.Fansly.APIDataTypes;
-
-internal abstract class BaseApiResponse {
-    [JsonPropertyName("success")]
-    public bool Success { get; set; } = false;
-}
--- Old Sublink/SubLinkKick/Kick/Events/ChatroomClearEvent.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Text.Json.Serialization;
-
-namespace xyz.yewnyx.SubLink.Kick.Events;
-
-public sealed class ChatroomClearEvent {
-    [JsonPropertyName("id")]
-    public string Id { get; set; } = string.Empty;
-}
